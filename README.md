51cf96652fce56c34855de32267a893e5f3734d4
Randy Sargent <randy.sargent@gmail.com>
2024-04-18 11:19:26 -0400

    Create 2020 Decennial census DHC description.json

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 
+
+
+## PREVIOUS, FOR 2020 Decennial Census
+
 To create new columns, run load_columns_2020.py, editing the "tables =" line to control which tables are loaded.  The notebook will create a new vector in the "census2020_block2020" directory for each table's column.
 
 Afterwards, rsync the columns to hal15
 
 The "screen" utility can be useful for managing long-running processes on the server.  See https://linuxize.com/post/how-to-use-linux-screen/ for a tutorial.
 
-
+For https://dotmaptiles.createlab.org/data/acs2020_5year_tract2020 to correctly show descriptions for all tables and columns, you need to create a description.json file using create_dotmaptiles_dataset_description_json.ipynb.
 
--- census_utils.py
+++ census_utils.py
@@ -2,13 +2,20 @@ import re
 
 # convert e.g. p35e to p035e
 def canonicalize_census_table_name(name):
-    (prefix, num, suffix) = re.match(r'([a-z]+)(\d+)([a-z])?$', name).groups()
+    match = re.match(r'([a-z]+)(\d+)([a-z]*)$', name, re.IGNORECASE)
+    if not match:
+        raise ValueError(f'Invalid table name: {name}')
+    (prefix, num, suffix) = match.groups()
     if not suffix:
         suffix = ''
     return f'{prefix}{int(num):03d}{suffix}'.upper()
 
 def canonicalize_census_column_name(name):
-    (num, suffix) = re.match(r'(\d+)([a-z]+)?$', name).groups()
+
+    match = re.match(r'(\d+)([a-z]+)?$', name, re.IGNORECASE)
+    if not match:
+        raise ValueError(f'Invalid column name: {name}')
+    (num, suffix) = match.groups()
     if not suffix or suffix == 'n':
         suffix = ''
 
